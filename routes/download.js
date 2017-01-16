8976c0b8bd23ccb9d3ae81921c208721a7cb8ba0
jaridwade <jarid.wade@icloud.com>
2017-01-16 15:26:32 -0500

    deploy

--- routes/download.js
+++ routes/download.js
@@ -2,16 +2,16 @@
 
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
+// var fs = require('fs');
 
 router.get('/', function(req, res) {
   res.download('./output/StatusGram.pdf');
 
-  if (process.env.COMPUTERNAME !== 'A004651') {
-    res.on('finish', function() {
-      fs.unlinkSync('./output/StatusGram.pdf');
-    });
-  }
+  // if (process.env.COMPUTERNAME !== 'A004651') {
+  //   res.on('finish', function() {
+  //     fs.unlinkSync('./output/StatusGram.pdf');
+  //   });
+  // }
   
 });
 
